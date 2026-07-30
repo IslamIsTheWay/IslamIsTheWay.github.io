5d1e81f2928b04d8a5a1891571af01aec1e51e0e
Ammar-052 <ammarwalidyounis@gmail.com>
2026-07-30 11:38:44 +0800

    Fix version-stamp pattern so i18n.js is also cache-busted (filenames with digits were skipped)

--- bump-version.sh
+++ bump-version.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 V=$(date +%Y%m%d%H%M)
 for f in *.html; do
   sed -i -E 's#(href="css/style\.css)(\?v=[0-9]+)?"#\1?v='"$V"'"#g' "$f"
-  sed -i -E 's#(src="js/[a-z]+\.js)(\?v=[0-9]+)?"#\1?v='"$V"'"#g' "$f"
+  sed -i -E 's#(src="js/[a-z0-9]+\.js)(\?v=[0-9]+)?"#\1?v='"$V"'"#g' "$f"
 done
 echo "assets stamped v=$V"
